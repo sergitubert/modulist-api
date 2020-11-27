8ae56a79f04b21ff559b6c7b2eef97f50a2028d4
Sergi Tubert <sergi.tubert@primer-impacto.com>
2020-11-27 23:52:49 +0100

    Add dependencies. Add cookie config. Add config middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as session from 'express-session';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(cookieParser());
-  await app.listen(3000);
+  // TODO: SET STORE IN REDIS
+  const sessionConfig: session.SessionOptions = {
+    secret: 'my-secret',
+    resave: false,
+    name: 'session',
+    cookie: { maxAge: 24 * 60 * 60 * 100 },
+    saveUninitialized: false,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    app.set('trust proxy', 1);
+    sessionConfig.cookie.secure = true;
+  }
+  app.use(helmet());
+  app.use(session(sessionConfig))
+  await app.listen(3001);
 }
 bootstrap();
