7e8e7252c4b09a0e38928fde4b2e34c54725ac7e
Sergi Tubert <sergi.tubert@primer-impacto.com>
2020-11-28 17:42:28 +0100

    Remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
@@ -20,7 +18,7 @@ import { UsersModule } from './users/users.module';
         AuthModule,
         UsersModule
     ],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule { }
